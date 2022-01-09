c80b282b7802fbfc198a8359eaaca18b69aa7f80
JunggeunIm <junggeun.im@dslab.global>
2022-01-09 17:43:48 +0900

    로그인 버튼 & 모달 추가 & home background 설정

--- frontEnd/nextjs-blog/pages/index.js
+++ frontEnd/nextjs-blog/pages/index.js
@@ -1,8 +1,21 @@
+import { useState } from "react";
 import Head from "next/head";
+
+import LoginModal from "../components/LoginModal";
 
 import ArrowForwardIcon from "@mui/icons-material/ArrowForward";
 
 export default function Home() {
+  const [isLoginModalOpen, setIsLoginModalOpen] = useState(false);
+
+  const handleClickLoginModalOpen = () => {
+    setIsLoginModalOpen(true);
+  };
+
+  const handleLoginModalClose = () => {
+    setIsLoginModalOpen(false);
+  };
+
   return (
     <div className="container">
       <Head>
@@ -11,16 +24,16 @@ export default function Home() {
       </Head>
 
       <main>
-        <h1 className="title">Welcome to NOSTRESS!</h1>
-
-        <p className="description">
-          스트레스는 만병의 근원! 아무 생각 없이 끄적여보는 걸 추천드립니다.
-        </p>
-
-        <div className="main--content">
-          <p className="main--content__imoji">&#x1F64B;</p>
-          <p className="main--content__text">
-            <a href="#" rel="noopener noreferrer">
+        <div className="home--container">
+          <h1 className="title">Welcome to NOSTRESS!</h1>
+          <p className="description">
+            스트레스는 만병의 근원! 아무 생각 없이 끄적여보는 걸 추천드립니다.
+          </p>
+          <div className="main--content">
+            <p
+              className="main--content__text"
+              onClick={handleClickLoginModalOpen}
+            >
               <span>로그인하고 끄적이러 가기</span>
               <span className="main--content__icon">
                 <ArrowForwardIcon
@@ -28,8 +41,8 @@ export default function Home() {
                   style={{ verticalAlign: "text-bottom" }}
                 />
               </span>
-            </a>
-          </p>
+            </p>
+          </div>
         </div>
       </main>
 
@@ -37,18 +50,39 @@ export default function Home() {
         <p>COPYRIGHT ⓒ 2022 NOSTRESS ALL RIGHTS RESERVED.</p>
       </footer>
 
+      <div className="home--bg"></div>
+
+      <LoginModal
+        isOpen={isLoginModalOpen}
+        modalClose={handleLoginModalClose}
+      />
       <style jsx>{`
         .container {
           min-height: 100vh;
           padding: 0 0.5rem;
           display: flex;
+          positon: relative;
           flex-direction: column;
           justify-content: center;
           align-items: center;
         }
 
+        .home--container {
+        }
+
+        .home--bg {
+          position: absolute;
+          top: 0;
+          left: 0;
+          width: 100%;
+          height: 100%;
+          background: center / contain no-repeat url("/img/home_background.png");
+          z-index: -1;
+          opacity: 0.5;
+        }
+
         main {
-          padding: 5rem 0;
+          padding: 5rem 1rem;
           flex: 1;
           display: flex;
           flex-direction: column;
@@ -57,23 +91,24 @@ export default function Home() {
         }
 
         .main--content {
+          margin-top: 8rem;
           text-align: center;
-        }
-
-        .main--content__imoji {
-          font-size: 12rem;
-          margin: 0;
         }
 
         .main--content__text {
           font-size: 2rem;
           font-weight: bold;
+          cursor: pointer;
+        }
+
+        .main--content__text:hover {
+          color: orange;
         }
 
         .main--content__icon {
           display: inline-block;
           transform: translateX(0);
-          transition: all 0.4s;
+          transition: transform 0.4s;
         }
 
         .main--content__text:hover .main--content__icon {
